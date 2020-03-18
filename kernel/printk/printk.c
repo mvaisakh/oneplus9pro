2deb86dd87a08faeff765e84c002c89a2f2df838
Park Ju Hyung <qkrwngud825@gmail.com>
2020-03-18 19:18:19 +0900

    printk: use buffer from the stack space
    Android uses way too much printk.
    
    Avoid allocating heap memory for lower overheads.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -864,7 +864,7 @@ int devkmsg_emit(int facility, int level, const char *fmt, ...)
 
 static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 {
-	char *buf, *line;
+	char buf[LOG_LINE_MAX + 1], *line;
 	int level = default_message_loglevel;
 	int facility = 1;	/* LOG_USER */
 	struct file *file = iocb->ki_filp;
@@ -885,15 +885,9 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			return ret;
 	}
 
-	buf = kmalloc(len+1, GFP_KERNEL);
-	if (buf == NULL)
-		return -ENOMEM;
-
 	buf[len] = '\0';
-	if (!copy_from_iter_full(buf, len, from)) {
-		kfree(buf);
+	if (!copy_from_iter_full(buf, len, from))
 		return -EFAULT;
-	}
 
 	/*
 	 * Extract and skip the syslog prefix <[0-9]*>. Coming from userspace
@@ -921,7 +915,6 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 	}
 
 	devkmsg_emit(facility, level, "%s", line);
-	kfree(buf);
 	return ret;
 }
 
@@ -1425,13 +1418,9 @@ static size_t msg_print_text(const struct printk_log *msg, bool syslog,
 
 static int syslog_print(char __user *buf, int size)
 {
-	char *text;
+	char text[LOG_LINE_MAX + PREFIX_MAX];
 	struct printk_log *msg;
 	int len = 0;
-
-	text = kmalloc(LOG_LINE_MAX + PREFIX_MAX, GFP_KERNEL);
-	if (!text)
-		return -ENOMEM;
 
 	while (size > 0) {
 		size_t n;
@@ -1488,7 +1477,6 @@ static int syslog_print(char __user *buf, int size)
 		buf += n;
 	}
 
-	kfree(text);
 	return len;
 }
 

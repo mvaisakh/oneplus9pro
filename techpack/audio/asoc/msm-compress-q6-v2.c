16cf3c15dcdffb65b0e5cad2acea561a4ccb0c3c
Vaisakh Murali <mvaisakh@statixos.com>
2022-06-02 18:51:03 +0000

    techpack/audio: msm-compress-q6-v2: Resolve implicit fallthrough warn
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- techpack/audio/asoc/msm-compress-q6-v2.c
+++ techpack/audio/asoc/msm-compress-q6-v2.c
@@ -938,7 +938,7 @@ static void compr_event_handler(uint32_t opcode,
 			prtd->sample_rate = sample_rate;
 		}
 	}
-		/* Fallthrough here */
+		/* fallthrough */
 	case APR_BASIC_RSP_RESULT: {
 		switch (payload[0]) {
 		case ASM_SESSION_CMD_RUN_V2:
@@ -2726,6 +2726,7 @@ static int msm_compr_trigger(struct snd_compr_stream *cstream, int cmd)
 #if !IS_ENABLED(CONFIG_AUDIO_QGKI)
 		spin_unlock_irqrestore(&prtd->lock, flags);
 #endif
+		/* fallthrough */
 	case SND_COMPR_TRIGGER_DRAIN:
 		pr_debug("%s: SNDRV_COMPRESS_DRAIN\n", __func__);
 		/* Make sure all the data is sent to DSP before sending EOS */

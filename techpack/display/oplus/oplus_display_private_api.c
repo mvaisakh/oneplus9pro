4aafdc90f2ba395b59a7142072dccc68b17187d5
jabashque <jabashque@gmail.com>
2023-01-09 15:09:51 -0800

    techpack: display: Don't skip LP1 cmd for AoD with fingerprint_mode on
    OPlus added extra code to skip sending the LP1/LP2 command if the
    fingerprint sensor is active. They probably have other changes they made
    in their fork of Android that requires this, but for us, this causes our
    panels to not actually enter LP1 mode at all when Ambient Display is
    enabled.
    
    Delete special case handling for this so that we can actually use LP1
    mode properly.
    
    Change-Id: Ib4a39f14df28ccbf21124defe6283f4a67837bb5
    Signed-off-by: Rahul K <spaced@rk134.cf>
    Signed-off-by: rk134 <me@rk134.cf>

--- techpack/display/oplus/oplus_display_private_api.c
+++ techpack/display/oplus/oplus_display_private_api.c
@@ -2875,37 +2875,6 @@ int dsi_display_oplus_set_power(struct drm_connector *connector,
 			set_oplus_display_scene(OPLUS_DISPLAY_AOD_SCENE);
 			break;
 
-		case OPLUS_DISPLAY_AOD_HBM_SCENE:
-
-			/* Skip aod off if fingerprintpress exist */
-			if (!sde_crtc_get_fingerprint_pressed(connector->state->crtc->state)) {
-				mutex_lock(&display->panel->panel_lock);
-				dsi_display_clk_ctrl(display->dsi_clk_handle,
-						     DSI_CORE_CLK, DSI_CLK_ON);
-
-				if (display->panel->panel_initialized) {
-					if ((!strcmp(display->panel->oplus_priv.vendor_name, "S6E3HC3") && (display->panel->panel_id2 >= 5)) ||
-						(!strcmp(display->panel->oplus_priv.vendor_name, "AMB670YF01") && (display->panel->panel_id2 >= 5))) {
-						rc = dsi_panel_tx_cmd_set(display->panel, DSI_CMD_AOD_HBM_OFF_PVT);
-					} else {
-						rc = dsi_panel_tx_cmd_set(display->panel, DSI_CMD_AOD_HBM_OFF);
-					}
-					mutex_unlock(&display->panel->panel_lock);
-					/* Update aod light mode and fix 3658965*/
-					mutex_lock(&display->panel->panel_lock);
-					oplus_update_aod_light_mode_unlock(display->panel);
-
-				} else {
-					pr_err("[%s][%d]failed to setting dsi command", __func__, __LINE__);
-				}
-
-				dsi_display_clk_ctrl(display->dsi_clk_handle,
-						     DSI_CORE_CLK, DSI_CLK_OFF);
-				mutex_unlock(&display->panel->panel_lock);
-				set_oplus_display_scene(OPLUS_DISPLAY_AOD_SCENE);
-			}
-
-			break;
 
 		case OPLUS_DISPLAY_AOD_SCENE:
 		default:
@@ -2954,31 +2923,8 @@ int dsi_display_oplus_set_power(struct drm_connector *connector,
 				}
 			}
 			#endif
-			if (sde_crtc_get_fingerprint_mode(connector->state->crtc->state) && oplus_dimlayer_hbm) {
-				mutex_lock(&display->panel->panel_lock);
-				dsi_display_clk_ctrl(display->dsi_clk_handle,
-							DSI_CORE_CLK, DSI_CLK_ON);
-
-				if (display->panel->panel_initialized) {
-					if ((!strcmp(display->panel->oplus_priv.vendor_name, "S6E3HC3") && (display->panel->panel_id2 >= 5)) ||
-						(!strcmp(display->panel->oplus_priv.vendor_name, "AMB670YF01") && (display->panel->panel_id2 >= 5))) {
-						rc = dsi_panel_tx_cmd_set(display->panel, DSI_CMD_AOD_HBM_ON_PVT);
-					} else {
-						rc = dsi_panel_tx_cmd_set(display->panel, DSI_CMD_AOD_HBM_ON);
-					}
-				} else {
-					pr_err("[%s][%d]failed to setting dsi command", __func__, __LINE__);
-				}
-
-				dsi_display_clk_ctrl(display->dsi_clk_handle,
-							DSI_CORE_CLK, DSI_CLK_OFF);
-				mutex_unlock(&display->panel->panel_lock);
-				set_oplus_display_scene(OPLUS_DISPLAY_AOD_HBM_SCENE);
-
-			} else {
-				rc = dsi_panel_set_nolp(display->panel);
-				set_oplus_display_scene(OPLUS_DISPLAY_NORMAL_SCENE);
-			}
+			rc = dsi_panel_set_nolp(display->panel);
+			set_oplus_display_scene(OPLUS_DISPLAY_NORMAL_SCENE);
 		}
 		if (!sde_crtc_get_fingerprint_mode(connector->state->crtc->state)) {
 			oplus_dsi_update_seed_mode(display);
